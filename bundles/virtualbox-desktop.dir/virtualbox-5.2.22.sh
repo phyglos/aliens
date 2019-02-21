8392e69732e9757f3bb6e3d9e9e3840dce4c1535
Alz <alz@phyglos.org>
2019-02-21 05:23:46 +0100

    Added virtualbox-desktop bundle

---
+++ bundles/virtualbox-desktop.dir/virtualbox-5.2.22.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+alien_install()
+{
+    case $BANDIT_TARGET_ARCH in
+    i?86)
+	sh ./VirtualBox-5.2.22-126460-Linux_x86.run
+	;;
+    x86_64)
+	sh ./VirtualBox-5.2.22-126460-Linux_amd64.run
+	;;
+    esac
+}
+
---
+++ bundles/virtualbox-desktop.dir/virtualbox-6.0.4.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+alien_install()
+{
+    sh ./VirtualBox-6.0.4-128413-Linux_amd64.run
+}
+
