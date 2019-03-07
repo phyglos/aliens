68103fe380c38211a5d934f8eefa39229b72220b
Alz <alz@phyglos.org>
2019-03-07 20:37:38 +0100

    Refactored and sanitized

--- bundles/virtualbox-desktop.dir/virtualbox-5.2.22.sh
+++ bundles/virtualbox-desktop.dir/virtualbox-5.2.22.sh
@@ -4,11 +4,11 @@ alien_install()
 {
     case $BANDIT_TARGET_ARCH in
     i?86)
-	sh ./VirtualBox-5.2.22-126460-Linux_x86.run
-	;;
+        sh ./VirtualBox-5.2.22-126460-Linux_x86.run
+        ;;
     x86_64)
-	sh ./VirtualBox-5.2.22-126460-Linux_amd64.run
-	;;
+        sh ./VirtualBox-5.2.22-126460-Linux_amd64.run
+        ;;
     esac
 }
 
