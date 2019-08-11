7a92ebff3d35abe8630c6d7bd855ad310fc9ae7b
Alz <alz@phyglos.org>
2019-08-11 10:39:41 +0200

    Upgraded to virtualbox-6.0.10

---
+++ bundles/virtualbox-desktop.dir/virtualbox-6.0.10.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+alien_install()
+{
+    sh ./VirtualBox-6.0.10-132072-Linux_amd64.run
+}
--- bundles/virtualbox-desktop.dir/virtualbox-6.0.8.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-alien_install()
-{
-    sh ./VirtualBox-6.0.8-130520-Linux_amd64.run
-}
-
