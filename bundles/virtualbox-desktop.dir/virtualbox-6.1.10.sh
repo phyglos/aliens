f93e0b49a636bf222791697f70a6bec053e2413c
alz-phyglos <alz@phyglos.org>
2020-07-19 08:17:01 +0200

    Upgraded to virtualbox 6.1.12

--- bundles/virtualbox-desktop.dir/virtualbox-6.1.10.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-alien_install()
-{
-    sh ./VirtualBox-6.1.10-138449-Linux_amd64.run
-}
---
+++ bundles/virtualbox-desktop.dir/virtualbox-6.1.12.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+alien_install()
+{
+    sh ./VirtualBox-6.1.12-139181-Linux_amd64.run
+}
